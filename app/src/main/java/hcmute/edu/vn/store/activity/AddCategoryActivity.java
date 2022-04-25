998eaedc7c408545ae7910008748b36b4a3b57e7
baolequoc <bao.lq@holistics.io>
2022-04-25 23:11:50 +0700

    add activity

---
+++ app/src/main/java/hcmute/edu/vn/store/activity/AddCategoryActivity.java
@@ -0,0 +1,14 @@
+package hcmute.edu.vn.store.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_category);
+    }
+}
